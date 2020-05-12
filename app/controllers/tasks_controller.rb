a8346d9bdd879d12e22d59cc23c92ee479c8596a
Joel Dickens <joeladickens@gmail.com>
2020-05-12 23:12:13 +0100

    didn't finish

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+class TasksController < ApplicationController
+  def home
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+#    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#  root to: 'tasks#home'
+#  get "tasks", to: "tasks#index", as: :tasks
+#  get "tasks/:id", to: "tasks#show", as: :task
+#  get "tasks/new", to: "tasks#new",  as: :new_task
+#
+#  post "tasks", to: "tasks#create"
+  resources :tasks
 end
+
